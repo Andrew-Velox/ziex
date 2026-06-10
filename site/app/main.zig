bf699c869b04f15e3778cb69a66cec537e4c8022
Nurul Huda (Apon) <me@nurulhudaapon.com>
2026-06-11 02:48:09 +0600

    docs: rm abs links

--- site/app/main.zig
+++ site/app/main.zig
@@ -15,10 +15,3 @@ pub const std_options = zx.std_options;
 pub const AppCtx = struct {
     port: u16,
 };
-
-pub const configs = .{
-    .main_site_url = if (builtin.mode == .Debug or builtin.os.tag == .wasi) "" else zx.info.homepage,
-    // Some examples are on the SSR site beacuse the main site is statically generated and some of examples depends on the SSR.
-    .ssr_url = if (builtin.mode == .Debug or builtin.os.tag == .wasi) "" else "https://ssr.ziex.dev",
-    .ssg_url = if (builtin.mode == .Debug or builtin.os.tag == .wasi) "" else zx.info.homepage,
-};
